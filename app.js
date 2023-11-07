851347cfeb2644806d9633b54d195b3ad3409bc0
Edgar Sanchez <egsanchez530@gmail.com>
2023-11-06 16:36:00 -0800

    Added Event Delegation for click event to button parent.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const numbers = document.querySelectorAll('.btn');
 const screen = document.querySelector('#screen-display');
 const deleteBtn = document.querySelector('.delete');
 const clearBtn = document.querySelector('.clear');
+const buttonHolder = document.querySelector('#buttons');
+const sum = document.querySelector('#sum');
+let displayValue = 0;
 // Operator functions
 
 const add = function (a, b) {
@@ -28,5 +31,12 @@ let num2;
 
 // Function that evaluates calc request
 function operate(num1, operator, num2) {
-  return operator(num1, num2);
+  return `${operator}`(num1, num2);
 }
+
+// Event Listeners
+
+buttonHolder.addEventListener('click', (e) => {
+  screen.innerText = e.target.innerText;
+  displayValue = parseInt(e.target.innerText);
+});
