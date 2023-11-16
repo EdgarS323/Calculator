2bd7977d796baee6947ec2092114b5d57dfcddfa
Edgar Sanchez <egsanchez530@gmail.com>
2023-11-16 12:37:57 -0800

    Added Event listner to update current display

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 //Grab Elements
 
-const numbers = document.querySelectorAll('num');
-const operators = document.querySelector('.operator');
-const previousDisplay = document.querySelector('#previous-input');
-const currentDisplay = document.querySelector('#current-input');
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+let previousDisplay = document.querySelector('#previous-input');
+let currentDisplay = document.querySelector('#current-input');
 const clearDisplay = document.querySelector('#clear');
 const deleteCharacter = document.querySelector('#delete');
+const sum = document.querySelector('#sum');
+
+// Variables for storing values
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+// Calculator Operations
 
 const add = function (a, b) {
   return a + b;
@@ -22,3 +30,20 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+// Functions
+
+const operation = (num1, operator, num2) => {
+  operator(num1, num2);
+};
+
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    num1 += e.target.innerText;
+    currentDisplay.textContent = num1;
+  });
+});
+
+operators.forEach((operand) => {
+  operand.addEventListener('click', (e) => {});
+});
